a104e271c6f66f79e01ecb3b2e241920402a25ce
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-02-16 15:41:45 +0700

    Chore: add validation for faculties endpoints
    - Add validation to all params in 'FacultiesController'.
    - Create 'faculties.dto.ts'.
    - Change 'HttpException' with specific class exception.
    - Improve API docs by adding @Api... decorator.

--- src/faculties/faculties.controller.ts
+++ src/faculties/faculties.controller.ts
@@ -1,88 +1,121 @@
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Param,
-} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Study } from 'src/studies/schemas/study.schema';
 import { StudiesService } from 'src/studies/studies.service';
 import { FacultiesService } from './faculties.service';
+import {
+  GetFacultiesByAbbrParams,
+  GetFacultiesByNameParams,
+  GetFacultyByCodeParams,
+  GetStudiesByFacultyParams,
+} from './faculty.dto';
 import { Faculty } from './schemas/faculty.schema';
 
 @ApiTags('Faculties')
 @Controller('faculties')
 export class FacultiesController {
   constructor(
-    private facultiesService: FacultiesService,
-    private studiesService: StudiesService,
+    private readonly facultiesService: FacultiesService,
+    private readonly studiesService: StudiesService,
   ) {}
 
+  @ApiOperation({ description: 'Get all faculties.' })
+  @ApiOkResponse({ description: 'Returns an array of faculties.' })
   @Get()
   async getFaculties(): Promise<Faculty[]> {
     return this.facultiesService.findAll();
   }
 
-  /**
-   * Get a faculty by its code.
-   * @param code The faculty code.
-   * @returns The suitable faculty.
-   */
-  @Get(':facultyCode')
-  async getFacultyByCode(@Param('facultyCode') code: string): Promise<Faculty> {
-    const suitableFaculty = await this.facultiesService.findByCode(code);
+  @ApiOperation({ description: 'Get a faculty.' })
+  @ApiOkResponse({ description: 'Returns a faculty.' })
+  @ApiBadRequestResponse({ description: 'When the code is invalid.' })
+  @ApiNotFoundResponse({ description: 'When no faculties match the code.' })
+  @ApiParam({
+    name: 'code',
+    type: 'string',
+    description: 'The faculty code.',
+    example: 'A',
+  })
+  @Get(':code')
+  async getFacultyByCode(
+    @Param() params: GetFacultyByCodeParams,
+  ): Promise<Faculty> {
+    const { code: facultyCode } = params;
+    const faculty = await this.facultiesService.findByCode(facultyCode);
 
     // Result validation.
-    if (suitableFaculty === null) {
-      throw new HttpException(
-        `No faculty found with the same code as '${code}'`,
-        HttpStatus.NOT_FOUND,
+    if (faculty === null)
+      throw new NotFoundException(
+        `No faculty found with code '${facultyCode}'`,
       );
-    }
 
-    return this.facultiesService.findByCode(code);
+    return faculty;
   }
 
-  /**
-   * Get all study programs by faculty code.
-   * @param code The faculty code.
-   * @returns The suitable study programs.
-   */
-  @Get(':facultyCode/studies')
-  async getStudiesByFaculty(@Param('facultyCode') code: string) {
+  @ApiOperation({ description: 'Get all study programs in a faculty.' })
+  @ApiOkResponse({ description: 'Returns an array of study program.' })
+  @ApiBadRequestResponse({ description: 'When the code is invalid.' })
+  @ApiNotFoundResponse({ description: 'When no faculties match the code.' })
+  @ApiParam({
+    name: 'code',
+    type: 'string',
+    description: 'The faculty code.',
+    example: 'A',
+  })
+  @Get(':code/studies')
+  async getStudiesByFaculty(
+    @Param() params: GetStudiesByFacultyParams,
+  ): Promise<Study[]> {
+    const { code: facultyCode } = params;
+
     // Validate the faculty code.
-    if (!(await this.facultiesService.isCodeExists(code))) {
-      throw new HttpException(
-        `No faculty found with the same code as '${code}'`,
-        HttpStatus.NOT_FOUND,
+    if ((await this.facultiesService.findByCode(facultyCode)) === null)
+      throw new NotFoundException(
+        `No faculty found with code '${facultyCode}'`,
       );
-    }
 
     // Get all study programs with the validated faculty code.
-    return this.studiesService.findByFaculty(code);
+    return this.studiesService.findByFaculty(facultyCode);
   }
 
-  /**
-   * Get all faculties with a suitable name.
-   * @param name The name of the faculty to be searched.
-   * @returns All faculties with matching names.
-   */
-  @Get('name/:facultyName')
+  @ApiOperation({ description: 'Find faculties by its name.' })
+  @ApiOkResponse({ description: 'Returns an array of faculty.' })
+  @ApiBadRequestResponse({ description: 'When the name is invalid.' })
+  @ApiParam({
+    name: 'name',
+    type: 'string',
+    description: 'The faculty name.',
+    example: 'kampus',
+  })
+  @Get('name/:name')
   async getFacultiesByName(
-    @Param('facultyName') name: string,
+    @Param() params: GetFacultiesByNameParams,
   ): Promise<Faculty[]> {
+    const { name } = params;
     return this.facultiesService.findByName(name);
   }
 
-  /**
-   * Find all faculties with its abbreviation.
-   * @param abbr The faculty abbreviation.
-   * @returns All faculties with matching abbreviation.
-   */
-  @Get('abbr/:facultyAbbr')
+  @ApiOperation({ description: 'Find faculties by its abbreviation.' })
+  @ApiOkResponse({ description: 'Returns an array of faculty.' })
+  @ApiBadRequestResponse({ description: 'When the abbreviation is invalid.' })
+  @ApiParam({
+    name: 'abbr',
+    type: 'string',
+    description: 'The faculty abbreviation.',
+    example: 'fpmipa',
+  })
+  @Get('abbr/:abbr')
   async getFacultiesByAbbr(
-    @Param('facultyAbbr') abbr: string,
+    @Param() params: GetFacultiesByAbbrParams,
   ): Promise<Faculty[]> {
+    const { abbr } = params;
     return this.facultiesService.findByAbbr(abbr);
   }
 }
---
+++ src/faculties/faculty.dto.ts
@@ -0,0 +1,25 @@
+import { IsAlpha, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class FacultyCodeParam {
+  @IsNotEmpty()
+  @IsAlpha()
+  @Length(1, 2)
+  code: string;
+}
+
+export class GetFacultyByCodeParams extends FacultyCodeParam {}
+
+export class GetStudiesByFacultyParams extends FacultyCodeParam {}
+
+export class GetFacultiesByNameParams {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3)
+  name: string;
+}
+
+export class GetFacultiesByAbbrParams {
+  @IsNotEmpty()
+  @IsString()
+  abbr: string;
+}
